8d1d497df07e178725cf1b4ae2d53974ed4d326f
Jeffrey Way <jeffrey@jeffrey-way.com>
2011-01-07 11:37:54 -0800

    Shortened if $audio check

--- templates/marketplaces_template.php
+++ templates/marketplaces_template.php
@@ -7,7 +7,7 @@
 <div class='tutorial_image'> 
 	<a href='$page'> $image </a>";
 		
-	if ( isset($audio) && $audio !== '' ) {
+	if ( !empty($audio) ) {
 		$template .= "<div> <a href='$audio'>Listen to a Preview</a> </div>";
 	}
 $template .= "
